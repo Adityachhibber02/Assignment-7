8a0f2e6693df2ea2d37bbce4e9150fecf56b0eeb
Aditya Chhibber <102915313+Adityachhibber02@users.noreply.github.com>
2022-07-16 19:27:38 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,36 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:\Users\adity\OneDrive\Desktop\Assignment 7.py ===========
+Enter size of list: 5
+Enter marks: 2
+Enter marks: 87
+Enter marks: 99
+Enter marks: 42
+Enter marks: 15
+[2, 15, 42, 87, 99]
+Enter size of array: 6
+Enter element 2
+Enter element 1
+Enter element 4
+Enter element 2
+Enter element 6
+Enter element 4
+[1, 2, 2, 4, 4, 6]
+Enter element to be searched and counted: 2
+Element is present at index 2
+Number of occurrences of element is 2
+Enter number of inputs:12
+Enter number:4
+Enter number:5
+Enter number:4
+Enter number:4
+Enter number:7
+Enter number:8
+Enter number:8
+Enter number:7
+Enter number:6
+Enter number:9
+Enter number:6
+Enter number:9
+[4, 5, 6, 7, 8, 9]
